ac3d626a3c955048203075a5d0b0c9fc93e215d8
n-kisyov <39015995+n-kisyov@users.noreply.github.com>
2021-09-13 11:54:17 +0300

    Add files via upload

---
+++ ledclock/ledclock.ino
@@ -0,0 +1,35 @@
+#include <MD_Parola.h>
+#include <MD_MAX72xx.h>
+#include <SPI.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUDP.h>
+#include <NTPClient.h>
+
+#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
+#define MAX_DEVICES 4
+
+#define CLK_PIN   D5
+#define DATA_PIN  D7
+#define CS_PIN    D8
+
+const char *ssid     = "";
+const char *password = "";
+
+const long utcOffsetInSeconds = 10800; // TZ offset in seconds
+
+
+MD_Parola P = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+
+// Arbitrary output pins
+// MD_Parola P = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
