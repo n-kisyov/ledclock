248f533246a2961405f108bf86065b979b2e7954
n-kisyov <39015995+n-kisyov@users.noreply.github.com>
2021-09-17 22:37:58 +0300

    OTA update
    OTA update

--- ledclock/ledclock.ino
+++ ledclock/ledclock.ino
@@ -4,6 +4,8 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUDP.h>
 #include <NTPClient.h>
+#include <ArduinoOTA.h>
+#include <ESP8266mDNS.h>
 
 #include "credentials.h"
 
@@ -43,8 +45,45 @@ void setup() {
     Serial.print ( "." );
   }
 
+
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+
+  ArduinoOTA.setPort(8266);
+  ArduinoOTA.setHostname("esp8266-ledclock");
+  ArduinoOTA.setPassword("password");
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
 
   P.displayText("WiFi OK", PA_CENTER, SPEED_TIME, PAUSE_TIME, PA_PRINT, PA_SCROLL_LEFT);
   P.displayAnimate();
@@ -54,14 +93,15 @@ void setup() {
 }
 
 void loop() {
+  ArduinoOTA.handle();
   if ((millis() - now) > 1000) {
     timeClient.update();
     h = timeClient.getHours();
     m = timeClient.getMinutes();
     now = millis();
-    Serial.print("** Time updated ** ");
-    Serial.print(h); Serial.print("h");
-    Serial.print(m); Serial.println("m");
+    Serial.print("Time updated : ");
+    Serial.print(h, DEC); Serial.print("h ");
+    Serial.print(m, DEC); Serial.println("m");
   }
 
   if (P.displayAnimate()) // animates and returns true when an animation is completed
